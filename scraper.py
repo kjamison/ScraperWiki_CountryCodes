8256bd0c34388ba822279efb5f26d4557dd3443c
Keith Jamison <keith.jamison@gmail.com>
2014-11-15 14:28:15 -0600

    make regional scrape optional

--- scraper.py
+++ scraper.py
@@ -6,6 +6,8 @@ import json
 
 from unidecode import unidecode
 
+run_regional = False
+
 def get_html(title):
     raw_json = scraperwiki.scrape("http://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + title)
     html = json.loads(raw_json)['parse']['text']['*']
@@ -18,14 +20,18 @@ doc = lxml.html.fromstring(html)
 for count, tr in enumerate(doc.cssselect('tr')):
     row = [(td.text_content()) for td in tr.cssselect('td')]
     if len(row)==5:
-        for ahref in tr.cssselect('a'):
-            detailink = ahref.attrib['href']
-            if detailink.find(':',0,len(detailink)) != -1:
-                detailink = detailink[6:]
-                print detailink
+        if run_regional:
+            for ahref in tr.cssselect('a'):
+                detailink = ahref.attrib['href']
+                if detailink.find(':',0,len(detailink)) != -1:
+                    detailink = detailink[6:]
+                    print detailink
         now = datetime.datetime.now()
         data ={"tmsp_scraped":str(now), "eng_short_name":row[0], "alpha_2_code":row[1], "alpha_3_code":row[2], "numeric_code":row[3], "iso_31662_code":detailink}
         scraperwiki.sqlite.save(unique_keys=["eng_short_name"], data=data, table_name="s_iso31661")
+        
+        if not run_regional:
+            continue
         
         html = get_html(detailink)
         doc = lxml.html.fromstring(html)
